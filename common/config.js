d277ddc4d05c3f8e0898eef1b618412f42c86599
RRapids <tengfeiqaq@gmail.com>
2020-11-09 15:19:41 +0800

    创建直播功能交互

--- common/config.js
+++ common/config.js
@@ -1,5 +1,10 @@
 export default {
 	baseUrl: "http://live2.utools.club",
 	socketUrl: "https://live2.utools.club",
-	imgUrl: "https://wanghuanle.oss-cn-beijing.aliyuncs.com/img/"
+	imgUrl: "https://wanghuanle.oss-cn-beijing.aliyuncs.com/img/",
+	
+	// 拉流前缀
+	livePlayBaseUrl:"http://47.242.155.95:23481",
+	// 推流前缀
+	livePushBaseUrl:"rtmp://47.242.155.95:23481"
 }
